6cc7e9521e332ca45ac92f1e73f3122b1971cdbe
Adrian Hawrylak <adrianhawrylak@MacBook-Air-Adrian.local>
2017-09-27 11:41:31 +0200

    Implement OutputGenerator - generate final response for user

---
+++ app/services/api_ai/output_generator.rb
@@ -0,0 +1,21 @@
+module ApiAi
+  class OutputGenerator
+    attr_reader :response
+
+    ACTION_PERFORMERS = {
+      gif: ApiAi::Performers::RandomGif,
+      create_todo: ApiAi::Performers::AddTodo,
+      show_todo_list: ApiAi::Performers::ShowTodoList
+    }.freeze
+
+    def initialize response
+      @response = response
+    end
+
+    def call
+      return { text: response[:output] } unless performer = ACTION_PERFORMERS[response[:action]]
+      performer.new(response).call
+    end
+
+  end
+end
