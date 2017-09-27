17a55b3d67368c51d471ba4189237f67f1aa1b72
ahawrylak <adrian.hawrylak@gmail.com>
2017-09-27 15:39:47 +0200

    Implement deleting todo tasks

--- app/services/api_ai/performers/delete_todo.rb
+++ app/services/api_ai/performers/delete_todo.rb
@@ -9,7 +9,14 @@ module ApiAi
       end
 
       def call
-        { text: 'I cant delete your todo yet :(' }
+        unless response[:action_incomplete]
+          user = User.find_by!(psid: response[:psid])
+          todo = user.todos.find_by(item: response[:parameters].fetch('task', ''))
+          return { text: 'No such task on the list ;(' } unless todo
+          todo.destroy!
+        end
+        message.reply(ApiAi::Performers::ShowTodoList.new(response, message).call)
+        { text: response[:output] }
       end
 
     end
