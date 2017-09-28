61e46b222c321f36d050d3316a57b343e82ca7f5
ahawrylak <adrian.hawrylak@gmail.com>
2017-09-28 14:52:19 +0200

    Get gif subject from user

--- app/services/api_ai/performers/random_gif.rb
+++ app/services/api_ai/performers/random_gif.rb
@@ -1,17 +1,21 @@
 module ApiAi
   module Performers
     class RandomGif
+      attr_reader :response, :message
+
       def initialize response, message = nil
         @response = response
         @message = message
       end
 
       def call
+        message.reply(text: 'Wait a second xD')
+        subject = response[:parameters].fetch('subject', nil) || 'random'
         {
           attachment: {
             type: 'image',
             payload: {
-              url: RandomGifUrl.new('doge').call
+              url: RandomGifUrl.new(subject).call
             }
           }
         }
