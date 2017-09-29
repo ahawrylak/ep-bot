ed8de0ec811968c4c4d204b5a7eb5fdad9a7ec07
ahawrylak <adrian.hawrylak@gmail.com>
2017-09-29 16:10:38 +0200

    Send pending messages to user when he gets online

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -3,7 +3,8 @@ include Facebook::Messenger
 Bot.on :message do |input|
   input.typing_on
 
-  User.first_or_create!(psid: input.sender['id'])
+  user = User.find_or_create_by(psid: input.sender['id'])
+  PendingMessagesSender.new(user, input).call
 
   api_ai_response = ApiAi::RequestSender.new(session_id: input.sender['id'], message: input.text).call
   parsed_response = ApiAi::ResponseParser.new(api_ai_response).call
